8934d7c7ca194d9e59fe0c655768c35f55a0b867
Hai Eliasi <eliasihai@hotmail.com>
2020-06-27 02:42:02 +0300

    Updates

---
+++ precache-manifest.d2f227ccd40f685f2b5e421c24f8c4a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4ff1d179cd746ef3905d1d386d5f39",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "07f4f7a2a53a5baae783",
+    "url": "/recipe-app/static/css/main.202fbf13.chunk.css"
+  },
+  {
+    "revision": "c5da3ec7b0cf454aeec6",
+    "url": "/recipe-app/static/js/2.baf085d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.baf085d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f4f7a2a53a5baae783",
+    "url": "/recipe-app/static/js/main.d6cb9554.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
